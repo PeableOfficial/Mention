e33d8594795620aec3c7aba115f023f8c669138c
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-03-12 02:32:23 +0100

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,13 @@ module.exports = {
     "prettier",
   ],
 
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.js"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+
   rules: {
     "react/react-in-jsx-scope": "off",
     "import/order": "off",
@@ -77,6 +84,24 @@ module.exports = {
       typescript: {
         alwaysTryTypes: true,
       },
+    },
+    tailwindcss: {
+      // These are the default values but feel free to customize
+      callees: ["classnames", "clsx", "ctl"],
+      config: "tailwind.config.js", // returned from `loadConfig()` utility if not provided
+      cssFiles: [
+        "**/*.css",
+        "!**/node_modules",
+        "!**/.*",
+        "!**/dist",
+        "!**/build",
+      ],
+      cssFilesRefreshRate: 5_000,
+      removeDuplicates: true,
+      skipClassAttribute: false,
+      whitelist: [],
+      tags: [], // can be set to e.g. ['tw'] for use in tw`bg-blue`
+      classRegex: "^class(Name)?$", // can be modified to support custom attributes. E.g. "^tw$" for `twin.macro`
     },
   },
 };
