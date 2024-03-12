86a5418502287c83466166fbefa1a0d91abe308f
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-03-12 22:52:39 +0100

    Added Vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import type { Viewport } from "next";
 import { cookies } from "next/headers";
