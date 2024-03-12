b885b4458c035f1c56d30b119895c9656b7f9769
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-03-12 03:50:57 +0100

    fixes

--- src/lib/socket-io.ts
+++ src/lib/socket-io.ts
@@ -3,7 +3,10 @@ import { io } from "socket.io-client";
 import { SOCKET_URL } from "@/config";
 
 const URL =
-  process.env.NODE_ENV === "production" ? SOCKET_URL : "http://localhost:8080";
+  /* process.env.NODE_ENV === "production" ? SOCKET_URL : "http://localhost:8080"; */
+  process.env.NODE_ENV === "production"
+    ? SOCKET_URL
+    : "https://mention-realtime-ef83ce1d7b5a.herokuapp.com";
 
 export const socket = io(URL, {
   autoConnect: false,
